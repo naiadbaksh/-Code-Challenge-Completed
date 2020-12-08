d47070e82b37848ad006e03230ba5676bbf91636
Naiad Baksh <65573342+naiadbaksh@users.noreply.github.com>
2020-12-07 20:34:34 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Chicken wings</li>
+      <li>Pasta</li>
+      <li>Chocolate chip cookies</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
